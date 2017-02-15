666342ba476115dd6a7200678a517eab2c7a30c3
chiguozi <qdlzr456@163.com>
2017-02-15 12:32:36 +0800

    BundleNameSetter

---
+++ Assets/Editor/AssetBundle/BundleNameSetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AssetCategory
+{
+	Prefab,
+	UI,
+	Font,
+	Effect,
+	Model,
+	Scene,
+	Script,
+	Shader,
+	Txt,
+}
+
+public class BundleNameSetter
+{
+
+}
