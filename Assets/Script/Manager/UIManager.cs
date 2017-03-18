32db99331c63f43ce23747c02abf312d30c5c5dd
chiguozi <qdlzr456@163.com>
2017-03-18 16:01:27 +0800

    uilayer

---
+++ Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class UILayer
+{
+	public const string nameLayer = "nameLayer";
+	public const string mainUILayer = "mainUILayer";
+}
