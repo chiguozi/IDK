3c84196e3fe46e1811d3c1db9ee64b5610910a30
chiguozi <qdlzr456@163.com>
2017-03-21 20:08:26 +0800

    push

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -6,4 +6,32 @@ public class UILayer
 {
 	public const string nameLayer = "nameLayer";
 	public const string mainUILayer = "mainUILayer";
+	public const string popupLayer = "popupLayer";
+	public const string loadingLayer = "loadingLayer";
+	public const string topLayer = "topLayer";
+}
+
+public class UIManager : SingleTon<UIManager>
+{
+	GameObject _uiRoot;
+	public GameObject uiRoot {get {return _uiRoot;}}
+	
+	Transform _canvasTransform;
+	
+	Canvas _mainCanvas;
+	public Canvas mainCanvas {get {return _mainCanvas;}}
+	
+	Camera _uiCamera;
+	public Camera uiCamera {get {return _uiCamera;}}
+	
+	Dictionary<string, Transform> _layerMap = new Dictionary<string, Transform>();
+	Dictionary<string, WndBase> _wndMap = new Dictionary<string, WndBase>();
+	
+	public void Init()
+	{
+		_uiRoot  = GameObject.Find("UI");
+		_canvasTransform = GameObject.Find("UI/MainCanvas").transform;
+		_mainCanvas = GameObject.Find("UI/MainCanvas").GetComponent<Canvas>();
+		_uiCamera = GameObject.Find("UI/UICamera").GetComponent<Camera>();
+	}
 }
