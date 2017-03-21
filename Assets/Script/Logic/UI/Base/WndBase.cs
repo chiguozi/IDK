87c938ca3f0a78fe392a9154048c5a2fd9f80452
chiguozi <qdlzr456@163.com>
2017-03-21 21:08:39 +0800

    wnd

---
+++ Assets/Script/Logic/UI/Base/WndBase.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WndBase
+{
+	string _defalutPath = "";
+	string defaultPath 
+	{
+		get
+		{
+			if(string.IsNullOrEmpty(_defalutPath))
+				_defalutPath = "Prebab/UI/" + GetType.Name;
+			return _defalutPath;
+		}
+	}
+	
+	GameObject _gameObject;
+	public GameObject gameObject {get {return _gameObject;}}
+	
+	Transform _transform;
+	public Transform transform {get {return _transform;}}
+	
+	RectTransform _rectTransform;
+	public RectTransform {get _rectTransform;}
+	
+	protected string _uiLayer = UILayer.popupLayer;
+	public string uiLayer {get {return _uiLayer;}}
+	
+	object[] _param;
+	
+	bool _isVisible = false;
+	protected string _path = null;
+	
+	Canvas = _canvas;
+	GraphicRaycaster _raycaster;
+	
+	public bool IsVisible
+	{
+		get {return _isVisible;}
+		set 
+		{
+			if(_isVisible == value)
+			{
+				//是否需要刷新界面
+				return;
+			}
+			_isVisible = value;
+			if(!_isVisible)
+			{
+				EventManager.Send(Events.UIEvent.OnWndOpen, this);
+			}
+			if(_gameObject == null)
+			{
+				if(string.IsNullOrEmpty(_path))
+					_path = defaultPath;
+				//@todo 依赖加载
+				//AssetBundleManager.ResLoad(_path, OnWndLoaded);
+			}
+			else
+			{
+				DoShowOrHide();
+			}
+			_gameObject.SetActive(_isVisible);
+		}
+	}
+	
+	void OnWndLoaded(object obj)
+	{
+		if(obj == null || _gameObject != null)
+			return;
+		_gameObject = GameObject.Instantiate(obj as GameObject) as GameObject;
+		_rectTransform = _gameObject.AddComponent<RectTransform>();
+		_transform = _gameObject.transform;
+		_canvas = _gameObject.AddComponent<Canvas>();
+		//部分面板可以不加？
+		_raycaster = _gameObject.AddComponent<GraphicRaycaster>();
+		EventManager.Send(Events.UIEvent.OnwWndLoaded, this);
+		InitView();
+		DoShowOrHide();
+		_gameObject.SetActive(_isVisible);
+		//设置renderorder
+	}
+	
+	private void DoShowOrHide()
+	{
+		if(_isVisible)
+		{
+			_transform.SetAsLastSibling();
+			OnShow();
+			EventManager.Send(Events.UIEvent.OnWndOpen, this);
+		}
+		else
+		{
+			OnHide();
+			EventManager.Send(Events.UIEvent.OnWndHide,this);
+		}
+	}
+	
+	public virtual void Show(object[] param)
+	{
+		_param = param;
+		IsVisible = true;
+	}
+	
+	public virtual void Hide()
+	{
+		IsVisible = false;
+	}
+	
+	public WndBase()
+	{
+		Init();
+	}
+	
+	protected virtual void Init()
+	{}
+	
+	protected virtual void InitView(){}
+	
+	protected virtual void OnShow() {}
+	protected virtual void OnHide() {}
+	
+	protected T GetComponent<T>(string path) where T : MonoBehaviour
+	{
+		var tChild = _transform.FindChild(path);
+		var com = tChild.GetComponent<T>();
+		return (T)com;
+	}
+	
+	protected RectTransform GetChildTransform(string path)
+	{
+		var tChild = _transform.FIndChild(path);
+		return tChild as RectTransform;
+	}
+	
+	protected GameObject GetGameObject(string path)
+	{
+		Transform t = _transform.FindChild(path);
+		return (null == t?) null : t.gameObject;
+	}
+}
--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -33,5 +33,57 @@ public class UIManager : SingleTon<UIManager>
 		_canvasTransform = GameObject.Find("UI/MainCanvas").transform;
 		_mainCanvas = GameObject.Find("UI/MainCanvas").GetComponent<Canvas>();
 		_uiCamera = GameObject.Find("UI/UICamera").GetComponent<Camera>();
+		InitLayer();
+		RegistEvent();
+	}
+	
+	void RegistEvent()
+	{
+		EventManager.Regist(Events.UIEvent.OnWndLoaded, OnWndLoaded);
+	}
+	
+	void OnWndLoaded(object obj)
+	{
+		var wnd = obj as WndBase;
+		wnd.transform.SetParent(_layerMap[wnd.uiLayer], false);
+	}
+	
+	void InitLayer()
+	{
+		AddLayer(UILayer.nameLayer);
+		AddLayer(UILayer.mainUILayer);
+		AddLayer(UILayer.popupLayer);
+		AddLayer(UILayer.loadingLayer);
+		AddLayer(UILayer.topLayer);
+	}
+	
+	void AddLayer(string name)
+	{
+		GameObject layer = new GameObject(name);
+		layer.transform.SetParent(_canvasTransform, false);
+		RectTransform rt = layer.AddComponent<RectTransform>();
+		rt.anchorMax = new Vector2(1,1);
+		rt.anchorMin = new Vector2(0,0);
+		rt.offsetMax = new Vector2(0,0);
+		rt.offsetMin = new Vector2(0,0);
+		_layerMap.Add(name, rt);
+	}
+	
+	public void Open<T>(params object[] param) where T : WndBase
+	{
+		var type = typeof(T);
+		if(!_wndMap.ContainsKey(type.Name))
+			_wndMap.Add(type.Name, Activator.CreateInstance<T>());
+		var wnd = _wndMap[type.Name];
+		wnd.Show(param);
+	}
+	
+	public void Hide<T>() where T : WndBase
+	{
+		var type = typeof(T);
+		if(!_wndMap.ContainsKey(type.Name))
+			return;
+		var wnd = _wndMap[type.Name];
+		wnd.Hide();
 	}
 }
