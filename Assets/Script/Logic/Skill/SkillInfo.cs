e7057a8aad3971c194538c83d629bcb17963d11f
chiguozi <qdlzr456@163.com>
2017-05-03 20:52:33 +0800

    skill

---
+++ Assets/Script/Logic/Skill/SkillInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillInfo
+{
+    //configSkill
+    public int skillId;
+    public int skillUid;
+    public string skillName;
+    public string iconUrl;
+    public bool lockTarget;
+    public bool faceTarget;
+    public float skillCd;
+    public float currentCd;
+
+    public List<SubSkill> subSkillInfoList = new List<SubSkill>();
+
+}
---
+++ Assets/Script/Logic/Skill/SubSkillInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubSkill
+{
+    public int id;
+    public float delay;
+    public List<int> skillActionInfoList = new List<int>();
+
+
+}
